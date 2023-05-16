610811e722490943dba852429a7b6ca81d3d0c99
Orlando Paulo <113864263+Orlando-Paulo99@users.noreply.github.com>
2023-05-16 12:14:52 -0300

    Update scricpt.js

--- scricpt.js
+++ scricpt.js
@@ -1,7 +1,31 @@
-document.write("<h2>"+"futebolll"+"</h2>");
+var nome;
+var email;
+
+
+function information(){
+    nome=prompt("Infore seu nome");
+    email=prompt("\nInfrome seu email");
+    
+    alert("Seja bem-vindo! "+nome);
+
+}
 
 function hello(){
-    var nome;
+   
     nome=prompt("qual é o seu nome");
     alert("hello "+nome);
+}
+
+function jogoone(){
+    
+    var jogo1="Palmeiras x Corinthias 16:40 diputa pela liderança";
+    
+    var jogo2="\nGremio x Internacional 16:00";
+    var jogo3="\nSantos x São Paulo";
+    alert(jogo1+jogo2+jogo3);
+}
+
+function galo(){
+    galinho="O Atletico Mineiro é u m dos maiores Brasil e o maior de Minas Gerais";
+    alert("olá,"+nome+"!"+"\n"+galinho);
 }
